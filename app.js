a1d8a4fd7d6f34083a0b5f34bcfb29dd85421be9
Bryant Wooters <jesusrox45@gmail.com>
2021-08-04 16:57:36 -0400

    Added api call

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require("https");
 const app = express();
 
+
+//This must be used to get data from a post
 app.use(bodyParser.urlencoded({extended: true}));
 
 //Code used to make photos available via Node
@@ -16,12 +19,37 @@ app.post("/signup", function(req,res){
     const firstName = req.body.fName;
     const lastName = req.body.lName;
     const email = req.body.email;
-    console.log(firstName, lastName, email);
-    
-    
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }    
+    const jsonData = JSON.stringify(data);
+    const apiKey = "2eb5f6713ca9ad3e7389e21c09561eaf-us5";
+    const audienceId = "71d7521168";
+    const url = `https://us5.api.mailchimp.com/3.0/lists/${audienceId}`;
+    const options = {
+        method: "POST",
+        auth: `Bryant Wooters:${apiKey}`,
+    }
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(data);
+        })
+    })
+    request.write(jsonData);
+    request.end();
 })
 
 app.listen(3000, function(req, res){
     console.log("Server is running on port 3000.");
 })
+
 
