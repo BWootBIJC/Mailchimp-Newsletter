bcac050f8a551498afed2a7bb201514eb5de22d2
Bryant Wooters <Jesusrox45@gmail.com>
2021-08-05 01:10:46 -0400

    Added success and failure signup pages

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const https = require("https");
 const app = express();
-import listId from 'authentication.js';
-import apiKey from 'authentication.js';
-
+const authentication = require("./authentication")
 
 
 //This must be used to get data from a post
@@ -35,19 +33,25 @@ app.post("/signup", function(req,res){
         ]
     }    
     const jsonData = JSON.stringify(data);
-    const url = `https://us5.api.mailchimp.com/3.0/lists/${listId}`;
+    const url = `https://us5.api.mailchimp.com/3.0/lists/${authentication.listId}`;
     const options = {
         method: "POST",
-        auth: `Bryant Wooters:${apiKey}`,
+        auth: `Bryant Wooters:${authentication.apiKey}`,
     }
     const request = https.request(url, options, function(response){
         response.on("data", function(data){
-            console.log(data);
+            if(response.statusCode >= '200' && response.statusCode <= '299'){
+                res.sendFile(__dirname + '/success.html');
+            }
+            else{
+                res.sendFile(__dirname + '/failure.html');
+            }
         })
     })
     request.write(jsonData);
     request.end();
 })
+
 
 app.listen(3000, function(req, res){
     console.log("Server is running on port 3000.");
